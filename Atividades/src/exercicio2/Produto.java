3d718d1e5882daf498ffa3483c41c9db5d5029aa
DeborahAguida <deaguida@hotmail.com>
2025-10-15 23:03:35 +0000

    Exercicio 2

---
+++ Atividades/src/exercicio2/Produto.java
@@ -0,0 +1,21 @@
+public class Produto {
+    private String nome;
+    private String codigo;
+    private int quantidade;
+    private double precoUnitario;
+
+    public Produto(String nome, String codigo, int quantidade, double precoUnitario){
+        this.nome = nome;
+        this.codigo = codigo;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade;}
+    public double getPrecoUnitario() { return precoUnitario; }
+    public void setPrecoUnitario(double precoUnitario) { this.precoUnitario = precoUnitario;}
+}
